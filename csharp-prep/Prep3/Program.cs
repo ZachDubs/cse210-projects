061c973ed7fc109719b8af4d283cdc615c8fe71d
ZachDubs <web21002@byui.edu>
2024-01-26 16:16:59 -0700

    fixed an issue where prep 3 was not finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
