9659e56c74214270a866e1cb7fe1040b51dc90ea
ZachDubs <web21002@byui.edu>
2024-01-26 15:48:47 -0700

    fixed an issue where prep 2 was not completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+
+         string letter = "";
+
+         if (grade >= 90.0)
+         {
+            letter = "A";
+         }
+         else if (grade >= 80 && grade < 90)
+         {
+            letter = "B";
+         }
+         else if (grade >= 70 && grade < 80)
+         {
+            letter = "C";
+         }
+         else if (grade >= 60 && grade < 70)
+         {
+            letter = "D";
+         }
+         else
+         {
+            letter = "F";
+         }
+
+         Console.WriteLine($"Your grade is: {letter}");
+
+         if (grade >= 70)
+         {
+            Console.Write("You passed! :)");
+         }
+         else
+         {
+            Console.Write("You did not pass :(");
+         }
     }
 }
