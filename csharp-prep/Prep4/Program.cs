d31a4746034a282d5e9031ec857a2a7f946aec5c
ZachDubs <web21002@byui.edu>
2024-01-26 17:00:01 -0700

    fixed an issue where prep 4 was not completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumbers = -1;
+
+        Console.WriteLine("Please enter your numbers. We will stop at 0. ");
+
+        while (userNumbers != 0)
+        {
+            Console.Write("Enter your number: ");
+            string numbersEntered = Console.ReadLine();
+            userNumbers = int.Parse(numbersEntered);
+
+            if (userNumbers != 0)
+            {
+            numbers.Add(userNumbers);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The total is: {sum}");
+
+        float average = (float)sum/numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int maximum = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        }
+
+        Console.Write($"The biggest number is: {maximum}");
     }
 }
